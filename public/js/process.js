cb10a1913bfbd8ab9ce78395db2802544cb1f846
Pedro <phkcond@gmail.com>
2017-05-08 20:52:53 -0300

    Merge branch 'develop' of https://github.com/matheus-garcia-leiturinha/lawsystem into develop
    # Conflicts:
    #	public/js/app.js
    #	public/js/clients.js
    #	public/js/datatables.js
    #	public/js/lib/bootstrap.min.js
    #	public/js/lib/html5shiv.min.js
    #	public/js/lib/jquery.min.js
    #	public/js/lib/respond.min.js
    #	public/js/process.js
    #	resources/assets/js/clients.js
    #	resources/views/base.blade.php
    #	webpack.mix.js

--- public/js/process.js
+++ public/js/process.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "./";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 24);
+/******/ 	return __webpack_require__(__webpack_require__.s = 26);
 /******/ })
 /************************************************************************/
 /******/ ({

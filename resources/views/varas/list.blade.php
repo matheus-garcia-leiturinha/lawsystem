1f969447fa279264e6759695514143e8230e2a96
Matheus Garcia <me@matheusgarcia.com>
2017-05-10 23:37:02 -0300

    Detalhes de Layout

--- resources/views/varas/list.blade.php
+++ resources/views/varas/list.blade.php
@@ -27,13 +27,13 @@
 
               @foreach ($varas as $vara)
                   <tr>
-                        <td>{{ $vara->id }}</td>
-                        <td>{{ $vara->nome }}</td>
-                        <td>{{ $vara->cidade }}</td>
-                        <td>
-                            <a href="{{ url('/varas/editar/'.$vara->id) }}"><i class="fa fa-edit"></i></a>
-                            <a href="{{ url('/varas/deletar/'.$vara->id) }}"><i class="fa fa-trash"></i></a>
-                        </td>
+                    <td>{{ $vara->id }}</td>
+                    <td>{{ $vara->nome }}</td>
+                    <td>{{ $vara->cidade }}</td>
+                    <td>
+                        <a href="{{ url('/varas/editar/'.$vara->id) }}"><i class="fa fa-edit"></i></a>
+                        <a href="{{ url('/varas/deletar/'.$vara->id) }}"><i class="fa fa-trash"></i></a>
+                    </td>
                   </tr>
               @endforeach
 

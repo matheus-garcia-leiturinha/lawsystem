cbcc8b9be382b927d428f2268b245110176ad60a
Matheus Garcia <me@matheusgarcia.com>
2017-07-27 22:47:20 -0300

    Merge branch 'develop' of https://github.com/matheus-garcia-leiturinha/lawsystem into develop
    # Conflicts:
    #	public/css/home.css
    #	public/css/layout.css

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -98,8 +98,17 @@ class ProcessController extends Controller
 
         $deposito_judicial  = $request->input('deposito_judicial');
 
+        $pericias           = $request->input('pericias');             // Se teve perícia
+        $pericia            = $request->input('pericia_natureza');     // Motivo da perícia
+        $value_pericia      = $request->input('pericia_honorario');    // Valor da perícia
 
+        $depositos          = $request->input('depositos');            // Se teve deposito
+        $deposito           = $request->input('deposito_motivo');      // Motivo da deposito
+        $value_deposito     = $request->input('deposito_valor');       // Valor da deposito
 
+        $recolhimentos      = $request->input('recolhimentos');        // Se teve recolhimento
+        $recolhimento       = $request->input('recolhimento_motivo');  // Motivo da recolhimento
+        $value_recolhimento = $request->input('recolhimento_valor');   // Valor da recolhimento
 
 
         $type_audiencia     = $request->input('type_audiencia');
@@ -171,10 +180,6 @@ class ProcessController extends Controller
             }
         }
 
-        $pericias           = $request->input('pericias');             // Se teve perícia
-        $pericia            = $request->input('pericia');              // Motivo da perícia
-        $value_pericia      = $request->input('value_pericia');        // Valor da perícia
-
         // Criação das Perícias do Processo
         if($pericias == 1 && isset($pericia))
         {
@@ -188,10 +193,6 @@ class ProcessController extends Controller
             }
         }
 
-        $depositos          = $request->input('depositos');            // Se teve deposito
-        $deposito           = $request->input('deposito');             // Motivo da deposito
-        $value_deposito     = $request->input('value_deposito');       // Valor da deposito
-
         // Criação dos Depositos do Processo
         if($depositos == 1 && isset($deposito))
         {
@@ -204,10 +205,6 @@ class ProcessController extends Controller
                 $pedido_processo->save();
             }
         }
-
-        $recolhimentos      = $request->input('recolhimentos');        // Se teve recolhimento
-        $recolhimento       = $request->input('recolhimento');         // Motivo da recolhimento
-        $value_recolhimento = $request->input('value_recolhimento');   // Valor da recolhimento
 
         // Criação dos Recolhimentos do Processo
         if($recolhimentos == 1 && isset($recolhimento))
